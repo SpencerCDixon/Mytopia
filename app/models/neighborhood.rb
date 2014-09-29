35cfeed0d0739c0de688e9dd84d583098750b03a
Spencer Dixon <spencercdixon@gmail.com>
2014-09-29 11:22:50 -0400

    Updated search functionality to be more specific

--- app/models/neighborhood.rb
+++ app/models/neighborhood.rb
@@ -6,8 +6,10 @@ class Neighborhood < ActiveRecord::Base
   has_many :reviews
 
   def self.search(search)
-    if search
-      where("name like ?", "%#{search}%")
+    if search.present?
+      where("name ilike :q or city ilike :q or state ilike :q", q: "%#{search}%")
+    else
+      self.all # Needs to be fixed 
     end
   end
 end
