c108eafe5cb6690d4edbedf55e9a0371693d39ff
Spencer Dixon <spencercdixon@gmail.com>
2014-09-29 11:05:35 -0400

    Basic search functionality is working

--- app/models/neighborhood.rb
+++ app/models/neighborhood.rb
@@ -4,4 +4,10 @@ class Neighborhood < ActiveRecord::Base
   validates :zipcode, presence: true
 
   has_many :reviews
+
+  def self.search(search)
+    if search
+      where("name like ?", "%#{search}%")
+    end
+  end
 end
