3640dac862d97b846a0247024b1f452962e10b26
Paul Brescia <brescia717@gmail.com>
2014-10-02 12:16:15 -0400

    1. Removed index from admin/reviews_controller.rb 2. Added tests for entering blank fields for admin comments & reviews specs

--- spec/features/admin_can_update_delete_reviews_spec.rb
+++ spec/features/admin_can_update_delete_reviews_spec.rb
@@ -20,5 +20,29 @@ feature "Admin can update or delete review" do
     expect(page).to have_content("New title")
   end
 
+  scenario "if admin updates review incorrectly, prompt with error message" do
+    review = FactoryGirl.create(:review)
 
+    sign_in_as(admin)
+    visit admin_neighborhood_path(review.neighborhood)
+
+    click_on "Edit"
+    expect(page).to have_content("Edit #{review.title}")
+
+    fill_in "Title", with: ""
+
+    click_on "Update Review"
+
+    expect(page).to have_content("prohibited")
+  end
+
+  scenario "admin can delete review" do
+    review = FactoryGirl.create(:review)
+
+    sign_in_as(admin)
+    visit admin_neighborhood_path(review.neighborhood)
+
+    click_on "Delete"
+    expect(page).to_not have_content(review.body)
+  end
 end
