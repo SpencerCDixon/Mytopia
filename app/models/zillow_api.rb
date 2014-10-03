b031dc9d2f55e9e339d922803595a9cced7d6d21
Spencer Dixon <spencercdixon@gmail.com>
2014-10-03 11:38:48 -0400

    Got informatino for geneeral and people complete

--- app/models/zillow_api.rb
+++ app/models/zillow_api.rb
@@ -27,4 +27,46 @@ attr_reader :neighborhood, :data
   def no_kids
     (people[:kids]["NoKids"].value.to_f * 100).round(2)
   end
+
+  def desc_income
+    people[:characteristics]["Income"]
+  end
+
+  def desc_employment
+    people[:characteristics]["Employment"]
+  end
+
+  def desc_education
+    people[:characteristics]["Education"]
+  end
+
+  def home_info
+    home_data = {}
+    home_data[:owners] = data.metrics["Home & Real Estate Data"]["Owners"][:neighborhood].value
+
+    home_data
+  end
+
+  def home_owners
+    (home_info[:owners].to_f * 100).round(2)
+  end
+
+  def median_income
+    @data.metrics["People Data"]["Median Household Income"][:neighborhood].value
+  end
+
+  def single_males
+    @data.metrics["People Data"]["Single Males"][:neighborhood].value.to_f * 100
+  end
+
+  def single_females
+    @data.metrics["People Data"]["Single Females"][:neighborhood].value.to_f * 100
+  end
+
+  def median_age
+    @data.metrics["People Data"]["Median Age"][:neighborhood].value
+  end
+
+
+
 end
